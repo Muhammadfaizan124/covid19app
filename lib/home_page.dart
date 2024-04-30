beb1ded2bfa74802440483ad3d5629dd9ef72f32
Muhammadfaizan124 <faizanislam124@gmail.com>
2024-05-01 02:18:52 +0500

    home page ready

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,12 +1,88 @@
+import 'package:covid19app/info_row.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:pie_chart/pie_chart.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
+  late final AnimationController controller =
+      AnimationController(duration: const Duration(seconds: 3), vsync: this)
+        ..repeat();
+  //colors for pie chart
+  final colorlist = [
+    const Color(0xffde5246),
+    const Color(0xff4285f4),
+    const Color(0xff1aa260),
+  ];
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.blue,
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: Colors.white70,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              PieChart(
+                dataMap: const {
+                  "Death": 5,
+                  "total": 20,
+                  "Recovered": 15,
+                },
+                chartRadius: MediaQuery.of(context).size.width / 2.2,
+                animationDuration: const Duration(seconds: 2),
+                colorList: colorlist,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    vertical: MediaQuery.of(context).size.height * 0.01),
+                child: Card(
+                  child: Column(
+                    children: [
+                      InfoRow(title: "death", value: "200"),
+                      InfoRow(title: "death", value: "200"),
+                      InfoRow(title: "death", value: "200"),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          minimumSize:
+                              Size(screenWidth * 1.0, screenHeight * 0.09),
+                        ),
+                        child: const Text(
+                          "search countries",
+                          style: TextStyle(
+                            fontSize: 19,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ lib/info_row.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+// ignore: must_be_immutable
+class InfoRow extends StatelessWidget {
+  String title, value;
+  InfoRow({super.key, required this.title, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(title),
+              Text(value),
+            ],
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          Divider(),
+        ],
+      ),
+    );
+  }
+}
