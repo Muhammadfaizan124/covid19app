b4218e63d357d2dd4812e2e6732c954560a9992b
Muhammadfaizan124 <faizanislam124@gmail.com>
2024-04-30 18:20:58 +0500

    connect with repo

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const covid19app());
+}
+
+class covid19app extends StatelessWidget {
+  const covid19app({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
